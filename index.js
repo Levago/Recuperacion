13e69898078b2ddbfc2cc6d93d8175e444f74e59
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-06-10 20:49:40 -0500

    add use of case to Hosting

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Hosting
 //Hoisting is a term that you will not find used in any specification prior to ECMAScript® 2015 Language Specification. The concept of hoisting was intended as a general way of referring to how execution contexts work in JavaScript (specifically the creation and execution phases). However, the concept can be a bit confusing at first.
+//case of use the array is being hosted.
 
 function nombreDelGato(nombre) {
     console.log("El nombre de mi gato es " + nombre);
