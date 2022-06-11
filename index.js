753140a8c971c418adde66f4e2cdd7b5415f48a3
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-06-10 20:45:14 -0500

    add use of case to FindIndex

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //FindIndex
 //The findIndex() method returns the index of the first element of an array that matches the given test function. Otherwise it returns -1.
+//case of use we are looking for values ​​in this array.
 const array1 = [5, 12, 8, 130, 44];
 
 const isLargeNumber = (element) => element > 13;
