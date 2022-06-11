4e42df5a2c43cc22c838478d2780b6d116e33d7e
Jorge Luis Marmolejo santos <jorgel.marmolejo96@hotmail.com>
2022-06-11 09:36:50 -0500

    create main branch and added index

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-//Concat
-//The concat() method is used to join two or more arrays. This method does not change existing arrays, but returns a new array.
-//case of use in this case we are going to unite these three arrangements.
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
-
-console.log(array3);
+//Recuperation Project
